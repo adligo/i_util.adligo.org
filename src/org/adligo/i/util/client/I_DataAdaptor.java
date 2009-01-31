3975251b0966804980f2b002a2e3c9e6acb98eba
scott <scott@adligo.com>
2009-01-31 23:58:37 +0000

    *** empty log message ***

--- src/org/adligo/i/util/client/I_DataAdaptor.java
+++ src/org/adligo/i/util/client/I_DataAdaptor.java
@@ -4,6 +4,10 @@ package org.adligo.i.util.client;
  * this interface represents a way to bind data back to the view
  * (I did this in the view on my GWT demo (MVC impl)
  * 
+ * Also I found this a nice way to implement the Event Handlers
+ * in the GWT demo (MVC impl) initally I used I_Invoker from the 
+ * ADI module but this always seems to return nothing
+ * 
  * @author scott
  *
  */
