e887038b0c5ced9523c106de44cb75749c81b228
scott <scott@adligo.com>
2009-11-17 07:05:38 +0000

    adding java.io.Serializable for classes that IsSerializable isn't enough

---
+++ src/org/adligo/i/util/client/I_Serializable.java
@@ -0,0 +1,9 @@
+package org.adligo.i.util.client;
+
+import java.io.Serializable;
+
+import com.google.gwt.user.client.rpc.IsSerializable;
+
+public interface I_Serializable extends IsSerializable, Serializable {
+
+}
