e62b1687501b1c8ce7374b3d402a757518c4269b
scott <scott@adligo.com>
2009-11-18 06:48:20 +0000

    removed gwt dependency as it is only necessary for IsSerializable which is redundant

--- src/org/adligo/i/util/client/I_Serializable.java
+++ src/org/adligo/i/util/client/I_Serializable.java
@@ -2,8 +2,6 @@ package org.adligo.i.util.client;
 
 import java.io.Serializable;
 
-import com.google.gwt.user.client.rpc.IsSerializable;
-
-public interface I_Serializable extends IsSerializable, Serializable {
+public interface I_Serializable extends Serializable {
 
 }
