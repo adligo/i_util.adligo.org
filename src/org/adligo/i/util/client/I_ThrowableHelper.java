c94d21a69603891c04a44c5edae75c890d6541c4
scott <scott@adligo.com>
2012-01-28 17:47:15 +0000

    this commit will break the entire adligo build, but is necessary to allow for indenting log messages in the new rolling_file_appender project

--- src/org/adligo/i/util/client/I_ThrowableHelper.java
+++ src/org/adligo/i/util/client/I_ThrowableHelper.java
@@ -3,4 +3,11 @@ package org.adligo.i.util.client;
 public interface I_ThrowableHelper {
 	public void fillInStackTrace(Throwable p);
 	public String getStackTraceAsString(Throwable p);
+	/**
+	 * @param preText allows for tabs (\t) do indent text
+	 * @param p
+	 * @param lineFeed allows for swapping out of unix or dos line feeds
+	 * @return
+	 */
+	public String getStackTraceAsString(String preText, Throwable p, String lineFeed);
 }
