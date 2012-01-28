80160a75fd9ad48cf251173df7383632ee12e664
scott <scott@adligo.com>
2012-01-28 17:59:23 +0000

    working on rolling_file_appender

--- src/org/adligo/i/util/client/I_ThrowableHelper.java
+++ src/org/adligo/i/util/client/I_ThrowableHelper.java
@@ -2,6 +2,14 @@ package org.adligo.i.util.client;
 
 public interface I_ThrowableHelper {
 	public void fillInStackTrace(Throwable p);
+	/**
+	 * @deprecated
+	 * use the getStackTraceAsString and pass in your own appender
+	 *    (jse and gwt have a StringBuilderAppender impl)
+	 *    (i_log has a StringBufferAppender for jme backward compatibility)
+	 * @param p
+	 * @return
+	 */
 	public String getStackTraceAsString(Throwable p);
 	/**
 	 * @param preText allows for tabs (\t) do indent text
