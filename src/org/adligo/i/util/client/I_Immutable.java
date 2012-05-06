961b9dfd0e55056ccac8d60933c8b968bbf8e55e
scott <scott@adligo.com>
2012-05-06 20:27:31 +0000

    added marker class to clarify xml_io immutables

---
+++ src/org/adligo/i/util/client/I_Immutable.java
@@ -0,0 +1,24 @@
+package org.adligo.i.util.client;
+
+import java.io.Serializable;
+
+/**
+ * this is simply a marker interface which shows the xml_io_generator
+ * project how to write immutable objects to and from xml.
+ * Classes which implement this interface should have a default no arg constructor
+ * and a constructor which has a argument with a single argument that can 
+ * be used as a constructor parameter to the type of the field with the name
+ * returned by getImmutableFieldName();
+ * 
+ * @author scott
+ *
+ */
+public interface I_Immutable extends Serializable {
+	/**
+	 * the java field name which should be used to serialize the object.
+	 * NOTE if this field is a String or other primitive wrapper (Integer, Long exc)
+	 * the toString method will be used when writing the xml.
+	 * @return
+	 */
+	public String getImmutableFieldName();
+}
