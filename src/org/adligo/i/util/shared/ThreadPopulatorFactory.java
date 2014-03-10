33b54cc81041b740e988b2977ab2f766469954ba
scott <scott@adligo.com>
2014-03-09 19:24:08 -0500

    some other cleanup

--- src/org/adligo/i/util/shared/ThreadPopulatorFactory.java
+++ src/org/adligo/i/util/shared/ThreadPopulatorFactory.java
@@ -1,16 +1,18 @@
 package org.adligo.i.util.shared;
 
 public class ThreadPopulatorFactory {
+	public static final String THREAD_POPULATOR_FACTORY_WAS_ALREADY_INITIALIZED = "ThreadPopulatorFactory was already initialized.";
+	public static final String THREAD_POPULATOR_FACTORY_NEEDS_A_NON_NULL_I_THREAD_POPULATOR_ARGUMENT = "ThreadPopulatorFactory needs a non null I_ThreadPopulator argument.";
 	protected static I_ThreadPopulator pop;
 	
 	protected synchronized static void init(I_ThreadPopulator tp) throws Exception {
 		if (tp == null) {
-			throw new Exception("ThreadPopulatorFactory needs a non null I_ThreadPopulator argument.");
+			throw new Exception(THREAD_POPULATOR_FACTORY_NEEDS_A_NON_NULL_I_THREAD_POPULATOR_ARGUMENT);
 		}
 		if (pop == null) {
 			pop = tp;
 		} else {
-			throw new Exception("ThreadPopulatorFactory was already initialized.");
+			throw new Exception(THREAD_POPULATOR_FACTORY_WAS_ALREADY_INITIALIZED);
 		}
 	}
 	
